b488a80d2f0d0451449d79d1fcdbcde815b4374a
Juan Carlos Valdés <idevkingos@gmail.com>
2020-06-19 15:45:13 -0300

    upload fotos firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDwy1_zPnjkp8yW6NKiW148tZeIfd5nF7Q',
+    authDomain: 'angularfotos-ad681.firebaseapp.com',
+    databaseURL: 'https://angularfotos-ad681.firebaseio.com',
+    projectId: 'angularfotos-ad681',
+    storageBucket: 'angularfotos-ad681.appspot.com',
+    messagingSenderId: '873799659378',
+    appId: '1:873799659378:web:ce95185b79be6a568d37eb'
+  }
 };
